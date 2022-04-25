afeb98ce78c49741e869735c9a32b90f8fbccf71
mogstuff <markmorgan.mcp@gmail.com>
2022-04-25 18:14:20 +0100

    feat: added placeholders for class methods and tests project

--- CheckoutLibb/Checkout.cs
+++ CheckoutLibb/Checkout.cs
@@ -4,6 +4,32 @@ namespace CheckoutLibb
 {
     public class Checkout
     {
+        public decimal GetTotal() 
+        {
+            return 0.00M;
+        }
+
+        public void ScanItem(Item item) 
+        {
+        
+        }
+
 
     }
+
+
+    public class Item
+    {
+        public string SKU { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+    public class SpecialOffer1
+    {
+        public string SKU { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal OfferPrice { get; set; }
+    }
+
 }
---
+++ CheckoutLibbTests/CheckoutTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutLibb;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutLibb.Tests
+{
+    [TestClass()]
+    public class CheckoutTests
+    {
+        [TestMethod()]
+        public void GetTotalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ScanItemTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
